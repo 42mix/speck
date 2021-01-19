bffe8b5cfad5f74936a0c8c68d382eedf8be40f5
Sachin Cherian <sachincs@protonmail.com>
2021-01-19 16:39:08 +0530

    slight optimizations

--- forecaster/forecaster.py
+++ forecaster/forecaster.py
@@ -7,9 +7,7 @@ class Forecatser:
         self.data = data
 
     def forecast_max_temp_c(self, date):
-        date_year = int(date.split('-')[0])
-        date_month = int(date.split('-')[1])
-        date_day = int(date.split('-')[2])
+        date_year, date_month, date_day = (int(i) for i in date.split('-'))
 
         weeks = [[[], []], [[], []], [[], []]]
 
@@ -18,13 +16,23 @@ class Forecatser:
             date = datetime(date_year, date_month, date_day)
 
             for j in range(-6, 1):
-                i[0].append(self.data.raw_data["{:04d}-{:02d}-{:02d} 00:00:00".format(date.year, date.month, (date - timedelta(days=1)).day)])
+                i[0].append(
+                    self.data.raw_data[
+                        "{:04d}-{:02d}-{:02d} 00:00:00".format(date.year, date.month, (date + timedelta(days=1)).day)
+                    ]
+                    ["maxtempC"]
+            )
 
             for j in range(1, 8):
-                i[1].append(self.data.raw_data["{:04d}-{:02d}-{:02d} 00:00:00".format(date.year, date.month, (date - timedelta(days=1)).day)])
+                i[1].append(
+                    self.data.raw_data[
+                        "{:04d}-{:02d}-{:02d} 00:00:00".format(date.year, date.month, (date + timedelta(days=1)).day)
+                    ]
+                    ["maxtempC"]
+            )
 
-        for i in range(len(weeks[0][0]) - 1):
-            print(weeks[0][0][i]["maxtempC"], weeks[1][0][i]["maxtempC"], weeks[2][0][i]["maxtempC"])        
+        for i in range(len(weeks[0][0])):
+            print(weeks[0][0][i], weeks[1][0][i], weeks[2][0][i])        
 
-        for i in range(len(weeks[0][1]) - 1):
-            print(weeks[0][1][i]["maxtempC"], weeks[1][1][i]["maxtempC"], weeks[2][1][i]["maxtempC"])    
+        for i in range(len(weeks[0][1])):
+            print(weeks[0][1][i], weeks[1][1][i], weeks[2][1][i])
