90c92c75c486ab3e89cb520cba87bd2fb8f17b1a
Sachin Cherian <sachincs@protonmail.com>
2021-02-01 15:06:50 +0530

    write example for current weather data

--- speck_frontend.py
+++
@@ -1,9 +0,0 @@
-from speck.forecaster import Forecaster as Fs
-import speck.errors as els
-from speck_gui.speck_gui import SpeckFrontend
-
-with open("token.txt", "r") as f:
-    token = f.read()
-
-speck_frontend_inst = SpeckFrontend(Fs(token))
-speck_frontend_inst.gui_main()
--- speck_gui/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import speck_gui
